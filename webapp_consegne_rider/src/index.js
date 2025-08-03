483299a4b30971f6869010f4991242b387992245
Webappwasabi <webappwasabi@gmail.com>
2025-08-03 14:00:27 +0200

    Add files via upload

---
+++ webapp_consegne_rider/src/index.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import ConsegneRider from './ConsegneRider';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <ConsegneRider />
+  </React.StrictMode>
+);
